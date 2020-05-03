2bf15c70baad72a3f7b6eaedb5bcf38d11f88bf5
Thomas Shortall <tshortall01@gmail.com>
2020-05-03 10:05:34 -0500

    Created Express Web Server

---
+++ app.js
@@ -0,0 +1,10 @@
+let express = require('express')
+let app = express();
+
+var port = 8080;
+
+app.get('/',(req,res,) => res.send('Hello World with Express'));
+
+app.listen(port, function(){
+    console.log("Running API on port " + port)
+})
