c1d749f90615275ea6d4e30236727e12559b9d44
Thomas Shortall <tshortall01@gmail.com>
2020-05-03 10:27:22 -0500

    working with mongoose and body-parser

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 let express = require('express')
 let app = express();
 let routes = require('./asgn-router');
+let bodyParser = require('body-parser');
+let mongoose = require('mongoose');
 
 var port = 8080;
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+mongoose.connect('mongodb://localhost/asgn-api',{
+    useNewUrlParser: true
+})
+var db = mongoose.connection;
+
+if (!db){
+    console.log("error")
+}else{
+    console.log("success")
+}
 
 app.use('/asgn-api', routes);
 
